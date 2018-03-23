d3b5b2112f73f62f081028315d4142ed1077816f
Clifton Pereira <pereiraclifton@gmail.com>
2018-03-23 15:27:30 -0400

    space

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # TODO: Implement function - Done
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     
-    correct_label = tf.reshape(correct_label, (-1,num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
 
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
 
